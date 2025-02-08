71ade0ab6e004fc4b0299db3b3aa125b835f2b27
Lyudmila Burbovskaya <l.burbovskaya@itransition.com>
2025-02-08 13:32:46 +0300

    fix: fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: 'src/__tests__/setup.ts',
+    watch: false,
     coverage: {
       include: ['**/*.tsx'],
       exclude: [
