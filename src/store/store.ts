3a8a687b0a2047954cc50fc63f7328989125c97d
Lyudmila Burbovskaya <l.burbovskaya@itransition.com>
2025-02-16 17:57:45 +0300

    feat: Add using RTK Query to make API calls

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+
 import selectedItemsReducer from './slices/selectedItemsSlice';
+import { peopleApi } from '../services/peopleApi.ts';
 
 export const store = configureStore({
   reducer: {
     selectedItems: selectedItemsReducer,
+    [peopleApi.reducerPath]: peopleApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(peopleApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
