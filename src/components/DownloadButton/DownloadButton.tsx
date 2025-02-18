7509047bab03000533d40be39135726cb636e979
Lyudmila Burbovskaya <l.burbovskaya@itransition.com>
2025-02-18 09:03:44 +0300

    fix: Fix csv

--- src/components/DownloadButton/DownloadButton.tsx
+++ src/components/DownloadButton/DownloadButton.tsx
@@ -9,14 +9,25 @@ export const DownloadButton = () => {
   );
 
   const handleDownload = () => {
-    const csvContent = selectedItems
-      .map(
-        (item) =>
-          `${item.name},${item.birth_year},${item.gender},${item.height},${item.mass}`
-      )
-      .join('\n');
+    const csvContent = [
+      'Name,Birth year,Gender,Height,Mass,Eye color,Hair color,Skin color',
+      ...selectedItems.map((item) =>
+        [
+          `"${item.name}"`,
+          `"${item.birth_year}"`,
+          `"${item.gender}"`,
+          `"${item.height}"`,
+          `"${item.mass}"`,
+          `"${item.eye_color}"`,
+          `"${item.hair_color}"`,
+          `"${item.skin_color}"`,
+        ].join(',')
+      ),
+    ].join('\n');
 
-    const blob = new Blob([csvContent], { type: 'text/csv' });
+    const blob = new Blob([csvContent], {
+      type: 'text/csv',
+    });
     const url = window.URL.createObjectURL(blob);
 
     if (linkRef.current) {
